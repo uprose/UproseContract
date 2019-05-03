821661a0f4db65e21a3775efde8a92badc287a21
Chintan Shah <chintan@wohlig.com>
2019-05-03 13:53:46 +0530

    changes

--- config/global.js
+++ config/global.js
@@ -2,5 +2,5 @@ global.Config = require("./config.js");
 global.BigchainDB = require("bigchaindb-driver");
 global.nacl = require("tweetnacl");
 global.base58 = require("bs58");
-global.API_PATH = "https://test.bigchaindb.com/api/v1/";
+global.API_PATH = "https://bigchain.wohlig.in/api/v1/";
 global.conn = new BigchainDB.Connection(API_PATH);
